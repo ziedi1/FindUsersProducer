2197e672bc3c1fd4535f98e1d8447837281e855f
ziedi <ziedi@linux-6zig>
2020-08-13 22:47:30 +0000

    the first commit

---
+++ src/main/java/producer/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package producer;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ *
+ * @author ziedi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String log4jConfPath = "/home/ziedi/NetBeansProjects/ReadLogFile/src/main/recources/log4j.properties";
+      PropertyConfigurator.configure(log4jConfPath);
+        TailProducer tailer = new TailProducer("/var/log/messages","localhost:9092","logfile" ,
+	31536000,500);
+    }
+    
+}
